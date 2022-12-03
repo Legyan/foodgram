a4a5f733127cdbf3f22d66521f77633e468c7a4b
Levon Gegamyan <levongeg@gmail.com>
2022-12-03 20:49:02 +0300

    favorites and shoppingcart links added to users in admin panel

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
+from recipes.models import Favorites, ShoppingCart
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.http import urlencode
+
 
 from users.models import User
 
@@ -9,10 +14,31 @@ class UserAdmin(admin.ModelAdmin):
     list_display = (
         'id', 'username',
         'first_name', 'last_name',
-        'email'
+        'email', 'favorites', 'shopping_cart'
     )
     search_fields = ('username', 'email')
     empty_value_display = '-пусто-'
+
+    def favorites(self, obj):
+        count = Favorites.objects.filter(user=obj).count()
+        url = (
+            reverse("admin:recipes_favorites_changelist")
+            + "?"
+            + urlencode({"user": f"{obj.id}"})
+        )
+        return format_html(f'<a href="{url}">{count} рецептов</a>')
+
+    def shopping_cart(self, obj):
+        count = ShoppingCart.objects.filter(user=obj).count()
+        url = (
+            reverse("admin:recipes_shoppingcart_changelist")
+            + "?"
+            + urlencode({"user": f"{obj.id}"})
+        )
+        return format_html(f'<a href="{url}">{count} рецептов</a>')
+
+    shopping_cart.short_description = "Список для покупок"
+    favorites.short_description = 'Избранное'
 
 
 admin.site.register(User, UserAdmin)
